776fe7375c88b97e4d8ec3fdbada5fe4cad4c5ac
Chukwuemeka Peter Eze <Chukwuemekapetereze@proton.me>
2026-08-06 08:58:17 +0100

    fix: add standalone output for Amplify server functions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "miro.medium.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn-images-1.medium.com",
+      },
+      {
+        protocol: "https",
+        hostname: "*.medium.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
